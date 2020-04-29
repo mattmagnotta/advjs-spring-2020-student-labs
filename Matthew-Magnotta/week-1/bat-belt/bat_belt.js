1c48b25a59586905b83a6e2e77f1a2ea97034658
Matthew Magnotta <mattmagnotta19d@icloud.com>
2020-04-28 19:10:01 -0700

    first push

---
+++ Matthew-Magnotta/week-1/bat-belt/bat_belt.js
@@ -0,0 +1,27 @@
+// function fforEach(list,fxn){
+//   for (let i = 0; i < list.length; i++){
+//     fxn(list[i])
+//   }
+// }
+
+
+
+// Produces a new array of values by running a fxn on each elemenet
+// function mmap(list, fxn){
+//   newList = []
+//   for (let i = 0; i < list.length; i++){
+//     newList.push(fxn(list[i]))
+// };
+//   return newList
+// }
+//
+// console.log(mmap([1,2,3,4],x => x*2))
+
+function ffilter(list,fxn){
+  filteredList = [];
+  for (let i = 0; i < list.length; i++){
+    fxn(list[i])
+
+  ]
+
+}
