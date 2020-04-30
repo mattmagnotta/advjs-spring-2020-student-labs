65df91a59dc68275998c536ed7fd012c9efdae0b
Matthew Magnotta <mattmagnotta19d@icloud.com>
2020-04-29 19:04:32 -0700

    started working on function_challenge

---
+++ Matthew-Magnotta/week-1/function_challenge/jasmine-standalone-3.5.0/src/functionChallenge.js
@@ -0,0 +1,25 @@
+
+
+// add function
+function add(a,b){
+  return a + b
+}
+
+// subtract function
+function sub(a,b){
+  return a - b
+}
+
+// multiply function
+function mul(a,b){
+  return a*b
+}
+// returns the arg given
+function identity(arg){
+  return arg
+}
+
+function identityf(arg){
+  return identity(arg)
+
+  }
